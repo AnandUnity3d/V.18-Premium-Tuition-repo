4748444f52fa5454a8227470f286a1a62c898417
Anand Biradar <31923258+AnandUnity3d@users.noreply.github.com>
2025-09-15 17:49:52 +0530

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -800,8 +800,8 @@ Designed for students of all classes are fully based on State Board, CBSE and IC
                 </div>
               </div>
             </div>
-          </div>
-        </div> */}
+          </div>*/}
+        </div> 
       </section>
 
       {/* Testimonials Section */}
