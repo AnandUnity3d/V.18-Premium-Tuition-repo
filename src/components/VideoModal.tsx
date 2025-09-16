e4a0ebd5381071c0e8cc4c42180a0a0eb9c15c90
Anand Biradar <31923258+AnandUnity3d@users.noreply.github.com>
2025-09-16 12:42:36 +0530

    Fix the correct VideoModal component

--- src/components/VideoModal.tsx
+++ src/components/VideoModal.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { X, Play } from 'lucide-react';
+import videoFile from './video.mp4';
 
 interface VideoModalProps {
   isOpen: boolean;
@@ -26,14 +27,7 @@ export default function VideoModal({ isOpen, onClose }: VideoModalProps) {
             autoPlay
             poster="https://images.pexels.com/photos/8199562/pexels-photo-8199562.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
           >
-            <source
-              src="https://sample-videos.com/zip/10/mp4/SampleVideo_1280x720_1mb.mp4"
-              type="video/mp4"
-            />
-            <source
-              src="https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
-              type="video/mp4"
-            />
+            <source src={videoFile} type="video/mp4" />
             Your browser does not support the video tag.
           </video>
           
