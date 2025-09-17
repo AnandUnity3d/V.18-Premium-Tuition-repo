7e4c3f27830ea827d68aa8caf288560614779d03
Anand Biradar <31923258+AnandUnity3d@users.noreply.github.com>
2025-09-17 16:06:33 +0530

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1028,9 +1028,7 @@ Designed for students of all classes are fully based on State Board, CBSE and IC
                       </div>
                     </div>
 
-                    <div className="group">
-                     
-                    </div>
+                  
                   </div>
                 </div>
               </div>
